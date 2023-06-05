5efa2a3edd4cf6da6944a7c06674f614311d1007
vardenis_pavardenis <epmc.vardenis.pavardenis@gmail.com>
2023-06-05 12:35:25 +0300

    003

---
+++ frontend/src/App.mjs
@@ -0,0 +1,10 @@
+import React from "react"
+
+const App = function ()
+{
+    const span = React.createElement("span", {}, "hola")
+
+    return span
+}
+
+export default App
--- frontend/src/index.mjs
+++ frontend/src/index.mjs
@@ -1,3 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.mjs"
+
 const element_id_root = document.getElementById("root")
 
-element_id_root.innerHTML = "hello"
+const root = ReactDOM.createRoot(element_id_root)
+
+const componet_App = React.createElement(App)
+
+root.render(componet_App)
