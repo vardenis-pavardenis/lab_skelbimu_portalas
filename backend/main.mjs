6c6241d01977ebd9a55c4664202519c618f74936
vardenis_pavardenis <epmc.vardenis.pavardenis@gmail.com>
2023-08-22 23:20:26 +0300

    007

--- backend/main.mjs
+++ backend/main.mjs
@@ -1,5 +1,4 @@
 import express from "express"
-import cookieParser from "cookie-parser"
 import controller_sign_up from "./controllers/controller_sign_up.mjs"
 
 const express_1 = express()
@@ -12,12 +11,9 @@ express_1.use(middleware_file_server)
 const middleware_parse_json_body = express.json({ limit: "4mb" })
 express_1.use(middleware_parse_json_body)
 
-const middleware_parse_cookies = cookieParser()
-express_1.use(middleware_parse_cookies)
-
 // endpoints
 
-express_1.post("/api/sign_up", controller_sign_up)
+express_1.post("/api/users/", controller_sign_up)
 
 // listen
 
